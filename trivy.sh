1fc24ed050bee79a539bce6e47b063ccb6010e52
0x70ssAM <hossamibraheem2014@gmail.com>
2026-03-29 14:20:05 +0300

    Final Script

--- trivy.sh
+++ trivy.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-# dockerImageName=$(awk 'NR==1 {print $2}' Dockerfile)
+dockerImageName=$(awk 'NR==1 {print $2}' Dockerfile)
 
-# echo "Scanning image: $dockerImageName"
+echo "Scanning image: $dockerImageName"
 
-# trivy image --exit-code 0 --severity HIGH --light $dockerImageName
+trivy image --exit-code 0 --severity HIGH --light $dockerImageName
 
-# trivy image --exit-code 1 --severity CRITICAL --light $dockerImageName
+trivy image --exit-code 1 --severity CRITICAL --light $dockerImageName
 
-IMAGE=$1
+# IMAGE=$1
 
-echo "Scanning $IMAGE"
+# echo "Scanning $IMAGE"
 
-trivy image --exit-code 0 --severity HIGH --light $IMAGE
-trivy image --exit-code 1 --severity CRITICAL --light $IMAGE
+# trivy image --exit-code 0 --severity HIGH --light $IMAGE
+# trivy image --exit-code 1 --severity CRITICAL --light $IMAGE
 exit_code=$?
 
 echo "Exit Code : $exit_code"
