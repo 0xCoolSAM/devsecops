b71a0b12e64c6163e8cd7ca45e91ff7d9fc4d5d4
0x70ssAM <hossamibraheem2014@gmail.com>
2026-03-29 14:34:47 +0300

    Final Script

--- trivy.sh
+++ trivy.sh
@@ -4,23 +4,18 @@ dockerImageName=$(awk 'NR==1 {print $2}' Dockerfile)
 
 echo "Scanning image: $dockerImageName"
 
-trivy image --exit-code 0 --severity HIGH --light $dockerImageName
+trivy image \
+  --severity HIGH,CRITICAL \
+  --exit-code 1 \
+  $dockerImageName
 
-trivy image --exit-code 1 --severity CRITICAL --light $dockerImageName
-
-# IMAGE=$1
-
-# echo "Scanning $IMAGE"
-
-# trivy image --exit-code 0 --severity HIGH --light $IMAGE
-# trivy image --exit-code 1 --severity CRITICAL --light $IMAGE
 exit_code=$?
 
 echo "Exit Code : $exit_code"
 
 if [ "$exit_code" -eq 1 ]; then
-    echo "Image scanning failed. Vulnerabilities found"
-    exit 1
+  echo "Image scanning failed. Vulnerabilities found"
+  exit 1
 else
-    echo "Image scanning passed. No CRITICAL vulnerabilities found"
+  echo "Image scanning passed. No HIGH or CRITICAL vulnerabilities found"
 fi
